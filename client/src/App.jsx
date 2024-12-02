a0715ba834e99e1fe33b894ed58161c526791534
bean <dbher.0010@gmail.com>
2024-12-03 01:13:48 +0900

    init: react 프로젝트 생성

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import { Button } from 'flowbite-react'; // Flowbite-React에서 제공하는 Button 컴포넌트 import
+
+function App() {
+    return (
+        <div className="App">
+            <Button color="blue">Flowbite React 버튼</Button>
+        </div>
+    );
+}
+
+export default App;
